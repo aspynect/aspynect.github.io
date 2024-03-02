9fc83d977397525f8b8942d559cb4eff4af1040d
PistachioPiper <peppypiper14@gmail.com>
2024-03-02 10:14:10 -0600

    fixed domain directions

--- !global_scripts/log.js
+++ !global_scripts/log.js
@@ -5,28 +5,28 @@
 //game, category, split count, timing methods
 function splitsLog(game, category, splitCount, timing) {
     if (timing[0] && timing[1]) {
-        fetch (`/api/splits?g=${encodeURIComponent(game)}&c=${encodeURIComponent(category)}&s=${encodeURIComponent(splitCount)}&t=${encodeURIComponent("RTA, IGT")}`)
+        fetch (`https://aspyn.gay/api/splits?g=${encodeURIComponent(game)}&c=${encodeURIComponent(category)}&s=${encodeURIComponent(splitCount)}&t=${encodeURIComponent("RTA, IGT")}`)
     } else if (timing[0]) {
-        fetch (`/api/splits?g=${encodeURIComponent(game)}&c=${encodeURIComponent(category)}&s=${encodeURIComponent(splitCount)}&t=${encodeURIComponent("RTA")}`)
+        fetch (`https://aspyn.gay/api/splits?g=${encodeURIComponent(game)}&c=${encodeURIComponent(category)}&s=${encodeURIComponent(splitCount)}&t=${encodeURIComponent("RTA")}`)
     } else {
-        fetch (`/api/splits?g=${encodeURIComponent(game)}&c=${encodeURIComponent(category)}&s=${encodeURIComponent(splitCount)}&t=${encodeURIComponent("IGT")}`)
+        fetch (`https://aspyn.gay/api/splits?g=${encodeURIComponent(game)}&c=${encodeURIComponent(category)}&s=${encodeURIComponent(splitCount)}&t=${encodeURIComponent("IGT")}`)
     }
 }
 
 //generation methods used
 function compGenLog(method) {
-    fetch (`/api/compconstruct?m=${method}`)
+    fetch (`https://aspyn.gay/api/compconstruct?m=${method}`)
 }
 
 //number of comparisons
 function compDownloadLog(count) {
-    fetch (`/api/compdownload?c=${count}`)
+    fetch (`https://aspyn.gay/api/compdownload?c=${count}`)
 }
 
 //which file was downloaded
 function coreDownloadLog() {
-    fetch (`/api/moddownload?f=LiveSplitCore`)
+    fetch (`https://aspyn.gay/api/moddownload?f=LiveSplitCore`)
 }
 function timerDownloadLog() {
-    fetch (`/api/moddownload?f=LiveSplitTimer`)
+    fetch (`https://aspyn.gay/api/moddownload?f=LiveSplitTimer`)
 }
